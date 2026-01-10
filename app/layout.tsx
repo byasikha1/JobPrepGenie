f82d8e2afacb4417ffb789d911960649d329a9d0
Sumit Halder <sumithalder100@gmail.com>
2026-01-10 21:35:09 +0530

    setup app, file and folder structure, routing and auth pages

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepGenius AI",
+  description: "AI-powered platform for preparing for mock interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body
+        className={`${monaSans.className} antialiased pattern`}
+        suppressHydrationWarning
+      >
+        {children}
+        <Toaster position="top-center" />
+      </body>
+    </html>
+  );
+}
---
+++ components/AuthForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/dist/client/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/dist/client/components/navigation";
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name:
+      type === "sign-up"
+        ? z.string().min(3, "Name must be at least 3 characters")
+        : z.string().optional(),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === "sign-up") {
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        console.log("SIGN IN", values);
+        toast.success("Signed in successfully!");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("Error during form submission:", error);
+      toast.error("An unexpected error occurred. ${error}");
+    }
+  }
+
+  const isSignIn = type === "sign-in";
+
+  return (
+    <div className="card-borer lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="Logo" width={32} height={38} />
+          <h2 className="text-primary-100">Prepwise</h2>
+        </div>
+
+        <h3>Practice job interview with AI</h3>
+
+        <FormProvider {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Enter your name"
+              />
+            )}
+            <FormField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="Enter your email"
+              type="email"
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
+
+            <Button type="submit" className="btn w-full mt-4">
+              {isSignIn ? "Sign In" : "Create an Account"}
+            </Button>
+          </form>
+        </FormProvider>
+        <p className="text-center text-sm text-muted-foreground mt-4">
+          {isSignIn ? "Don't have an account? " : "Already have an account? "}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="text-primary-100 hover:underline ml-1"
+          >
+            {!isSignIn ? "Sign in" : "Sign up"}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
