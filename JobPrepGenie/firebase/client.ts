3affd7147801b589c5a1e350d96bcbd5ec2c80cc
Shikha Bhatt <byasikha1@gmail.com>
2026-03-16 23:02:31 +0530

    final code push

---
+++ JobPrepGenie/firebase/client.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCp1qmx5Goz90016MfH4qH_O2PKWpvdXbY",
+  authDomain: "jobprepgenie.firebaseapp.com",
+  projectId: "jobprepgenie",
+  storageBucket: "jobprepgenie.firebasestorage.app",
+  messagingSenderId: "511425222242",
+  appId: "1:511425222242:web:315e667312ed64b390233b",
+  measurementId: "G-PFHYHKK2CV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,16 @@
+import Agent from '@/components/Agent'
+import { getCurrentuser } from '@/lib/actions/auth.action';
+
+const page = async () => {
+  const user = await getCurrentuser();
+
+  return (
+    <>
+        <h3>Interview Generation</h3>
+
+        <Agent userName= {user?.name} userId={user?.id} type="generate" />
+    </>
+  )
+}
+
+export default page
